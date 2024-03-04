79afc869853de7c8f44108d06d70acb46b1e07ec
Manh Hung (Benjamin) Nguyen <hungbenjamin402@gmail.com>
2024-03-04 13:19:07 -0800

    Initial Commit

---
+++ QuizScore/QuizScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizScoreApp.swift
+//  QuizScore
+//
+//  Created by benjamin on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct QuizScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
