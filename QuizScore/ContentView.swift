f9a8f7356021d03f10cc0d379c72cb06993e78b0
Manh Hung (Benjamin) Nguyen <hungbenjamin402@gmail.com>
2024-03-04 14:49:54 -0800

    First Demo
    1. Created Question Bank
    2. Created functional UI

--- QuizScore/ContentView.swift
+++ QuizScore/ContentView.swift
@@ -8,17 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    let questionAnswer = [
+        ["All spiders have eight eyes","False"],
+        ["A group of crows is called a murder", "True"],
+        ["Sharks can blink with both eyes", "False"],
+        ["Dolphins sleep with one eye open", "True"],
+        ["Ostriches can run faster than horses", "True"],
+        ["Bats are mammals", "True"],
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.white)
+                .ignoresSafeArea()
+            VStack{
+                Text("Quiz Time")
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
+                    .padding()
+                
+                ForEach(0..<2) { index in
+                    Text("Question \(index)/" + String(questionAnswer.count))
+                        .padding()
+                    Spacer()
+                    Text(questionAnswer[index][0])
+                    Spacer()
+                    
+                    HStack {
+                        AnswerButton(text: "\(questionAnswer[0][1])")
+                    }
+                }
+                
+                
+            }
         }
-        .padding()
+        
+        
     }
 }
 
+struct AnswerButton: View {
+    let text: String
+    var body: some View {
+        Button(action: {
+            print("You selected " + text)
+        }) {
+            Text(text)
+        }
+        .padding()
+        .border(Color.red, width: 10)
+    }
+}
 #Preview {
     ContentView()
 }
